ab4feec5998f23116087291a28bb03b87e16c9d8
Scroggs <35433561+Scroggs@users.noreply.github.com>
2018-01-14 14:03:26 -0800

    Add files via upload

--- Hex_converison_3.py
+++ Hex_converison_3.py
@@ -1,4 +1,46 @@
-import math
+import math    
+hex_ref = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "a",
+    11: "b",
+    12: "c",
+    13: "d",
+    14: "e",
+    15: "f"
+    }
+def play_again():
+    again = input("Would you like to convert another number? y/n ")
+    again = again.lower()
+    if again == "y" or again == "yes" or again == "":
+        conversion()
+    elif again == "n" or again == "no":
+        print ("Dilly Dilly, goodbye.")
+        exit()
+    else:
+        print ('Please choose "y" or "n" ')
+        play_again()
+
+def hex_conversion(address):
+    hex_number = []
+    if address == 0:
+        print ("Your hex number is: 0")
+        play_again()
+    else:
+        while address != 0:
+            x = math.trunc(address % 16)
+            hex_number.append(hex_ref[x])
+            address = math.trunc(address / 16)
+    print ("Your hex number is: " + "".join(reversed(hex_number)))
+    play_again()
 
 def conversion():
     address = input("Enter a number: ")
@@ -7,51 +49,11 @@ def conversion():
         conversion()
     else:
         address = int(address)
-    
-    hex_ref = {
-        0: "0",
-        1: "1",
-        2: "2",
-        3: "3",
-        4: "4",
-        5: "5",
-        6: "6",
-        7: "7",
-        8: "8",
-        9: "9",
-        10: "a",
-        11: "b",
-        12: "c",
-        13: "d",
-        14: "e",
-        15: "f"
-        }
-    
-    def hex_conversion(address):
-        hex_number = []
-        if address == 0:
-            print ("Your hex number is: 0")
-        else:
-            while address != 0:
-                x = math.trunc(address % 16)
-                hex_number.append(hex_ref[x])
-                address = math.trunc(address / 16)
-        print ("Your hex number is: " + "".join(reversed(hex_number)))
-        def play_again():
-            again = input("Would you like to convert another number? y/n ")
-            again = again.lower()
-            if again == "y" or again == "yes" or again == "":
-                conversion()
-            elif again == "n" or again == "no":
-                print ("Goodbye.")
-                exit()
-            else:
-                print ('Please choose "y" or "n" ')
-                play_again()
-        play_again()            
     hex_conversion(address)
 
 conversion()
-            
 
-            
+
+
+
+  
